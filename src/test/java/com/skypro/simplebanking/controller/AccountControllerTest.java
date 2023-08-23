c9eb5212a858e36a0f1b3125c5f19daddd87903b
ПК <ekrapostin@gmail.com>
2023-08-23 12:57:57 +0300

    На работает авторизация ошибка 401

--- src/test/java/com/skypro/simplebanking/controller/AccountControllerTest.java
+++ src/test/java/com/skypro/simplebanking/controller/AccountControllerTest.java
@@ -9,8 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.Base64Utils;
+
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,14 +34,21 @@ public class AccountControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Test
     void getAccountTest() throws Exception {
-        User user = new User("user", "pass");
+        User user = new User("user",passwordEncoder.encode("pass"));
         user = userRepository.save(user);
         Account account = new Account(AccountCurrency.USD, 1L,user);
         account =  accountRepository.save(account);
 
-        mockMvc.perform(get("/account/" +account.getId()))
+        String base64Encoded = Base64Utils.encodeToString((user.getUsername() + ":" + "pass")
+                .getBytes(StandardCharsets.UTF_8));
+        mockMvc.perform(get("/account/" +account.getId())
+                        .header(HttpHeaders.AUTHORIZATION, "Basic " + base64Encoded))
                 .andExpect(status().isOk());
     }
 
